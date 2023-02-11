a00c9f5f1cea5b56d2ef0abe59661277a015b3d1
Alfredo <alfredo.austin@ui.ac.id>
2023-02-11 16:10:10 +0700

    Removing dysfunctional file

--- utils/utils.ts
+++
@@ -1,5 +0,0 @@
-import { useLocalStorage } from '@/Hook/useLocalStorage';
-import { RawNote } from '@/utils/types';
-
-const [notes, setNotes] = useLocalStorage<RawNote>('NOTES', []);
-export function handle() {}
